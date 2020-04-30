3987379896f6696719f8bdb5f665d8a729264f6a
Felipe Zêba <51774420+felipe-zg@users.noreply.github.com>
2020-04-30 18:28:46 -0300

    'rotas'

---
+++ src/Pages/Signin/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, View } from 'react-native';
+
+import {Container} from '../../Styles';
+
+// import { Container } from './styles';
+
+export default function Signin() {
+  return (
+    <Container>
+
+    </Container>
+  );
+}
