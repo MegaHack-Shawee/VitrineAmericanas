3bf7e15dfd03df2f7c44f6ab44b8c17db11dfc24
gabrielrportugal <gabrielrportugal@outlook.com>
2020-04-30 19:41:54 -0300

    Conflitos

--- src/Pages/Signin/index.js
+++ src/Pages/Signin/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import {SafeAreaView, View } from 'react-native';
+import {SafeAreaView, View} from 'react-native';
 
 import {Container} from '../../Styles';
 
-// import { Container } from './styles';
+//import { Container } from './styles';
 
 export default function Signin() {
-  return (
-    <Container>
-
-    </Container>
-  );
+  return <Container />;
 }
--- src/Pages/Signin/styles.js
+++ src/Pages/Signin/styles.js
@@ -1,5 +1,3 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.SafeAreaView`
-  
-`;
+export const Container = styled.SafeAreaView``;
