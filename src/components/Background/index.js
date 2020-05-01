9446aea49730aaed551b7be7042713eb7120ed12
gabrielrportugal <gabrielrportugal@outlook.com>
2020-04-30 21:51:46 -0300

    App

--- src/components/Background/index.js
+++
@@ -1,8 +0,0 @@
-import LinearGradient from 'react-native-linear-gradient';
-import styled from 'styled-components/native';
-
-export default styled(LinearGradient).attrs({
-    colors: ['#F57C00', '#D84315'],
-})`
-    flex: 1;
-`;
