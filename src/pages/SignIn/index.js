b5b3ad088858408b63532215dbce2f4f184fb8b3
gabrielrportugal <gabrielrportugal@outlook.com>
2020-05-04 15:04:29 -0300

    Trocando cor dos inputs SignIn

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -69,7 +69,7 @@ export default function SignIn({navigation}) {
                         <Logo source={logoVitrineAmericanas} />
                     </ImageView>
                     <ViewInput>
-                        <Icon name="mail" size={20} color="#ff9000" />
+                        <Icon name="mail" size={24} color="#4f4f4f" />
                         <TextInput
                             value={email}
                             onChangeText={setEmail}
@@ -82,7 +82,7 @@ export default function SignIn({navigation}) {
                         />
                     </ViewInput>
                     <ViewInput>
-                        <Icon name="lock" size={20} color="#ff9000" />
+                        <Icon name="lock" size={24} color="#4f4f4f" />
                         <TextInput
                             ref={passRef}
                             value={password}
