694d8419beb9bdda1fd20fa3b89e2852c9c1aa76
higorhms <higorhmscebrac@hotmail.com>
2020-05-03 23:34:48 -0300

    Code review

--- src/pages/LoadData/index.js
+++ src/pages/LoadData/index.js
@@ -1,23 +1,27 @@
 import React, {useEffect} from 'react';
-import {View} from 'react-native';
 import auth from '@react-native-firebase/auth';
 import database from '@react-native-firebase/database';
 import {useDispatch} from 'react-redux';
 
-import {Container} from './styles';
 import {startAddresses} from '../../store/modules/Address/actions';
 import Background from '../../components/Background';
 import Logo from '../../components/Logo';
 
+import {Container} from './styles';
+
 export default function LoadData({navigation}) {
     const dispatch = useDispatch();
+
     useEffect(() => {
         const user = auth().currentUser;
         if (user != null) {
             const ref = database().ref(`/addresses/${user.uid}`);
             ref.orderByKey().once('value', onAddressesSnapshot);
         }
-        navigation.replace(user ? 'HomeScreen' : 'SignInScreen');
+
+        setTimeout(() => {
+            navigation.replace(user ? 'HomeScreen' : 'SignInScreen');
+        }, 2000);
     });
 
     function onAddressesSnapshot(snapshot) {
